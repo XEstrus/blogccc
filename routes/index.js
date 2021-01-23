b6fe54341d8be3941cd207ff414365799864e7b7
blogccc <921299380@qq.com>
2021-01-23 15:45:50 +0800

    yemian

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Express'});
 });
+//首页路由配置
+// router.get('/index',function(req,res){
+//   res.render('index',{})
+// });
 //登录路由配置
 router.get('/login',function(req,res){
   res.render('login',{})
@@ -13,9 +17,9 @@ router.get('/login',function(req,res){
 router.get('/details',function(req,res){
   res.render('details',{})
 })
-//文章页路由配置
-router.get('/xwz',function(req,res){
-  res.render('xwz',{})
+//写文章页路由配置
+router.get('/write',function(req,res){
+  res.render('write',{})
 })
 //文章注册页路由配置
 router.get('/zhuce',function(req,res){
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//实现用户提交信息，注册时间
+//Response 响应
+//Request  请求
+router.post('/addUser',(req,res,next)=>{
+  //console.log(req.body.userName);
+  //用户填写的表单信息可以通过req.body获取到
+  res.send('点击注册了')
+  console.log(req.body.userName)
+  console.log(req.body.password)
+  console.log(req.body.passwordC)
+})
 module.exports = router;
